e667baaba67aeec8bc7b5fea418bb5ced7b68e1c
= <=>
2023-02-01 07:41:58 -0500

    agrengando las especificacione para crear la api

---
+++ app/Http/Controllers/BalanceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use Illuminate\Http\Request;
+
+class BalanceController extends Controller
+{
+    //
+    public function show(Request $request , Account $account){
+
+    }
+}
